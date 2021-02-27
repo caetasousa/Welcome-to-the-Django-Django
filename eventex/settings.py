319f265bc6e388d0fefbff95dbe45b1787ea431e
Eduardo <caetasousa@gmail.com>
2021-02-27 13:46:42 -0300

    Import project

--- eventex/settings.py
+++ eventex/settings.py
@@ -76,9 +76,9 @@ WSGI_APPLICATION = 'eventex.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-default_url = 'sqlite///' + str(BASE_DIR / 'db.sqlite3')
+default_dburl = 'sqlite///' + str(BASE_DIR / 'db.sqlite3')
 DATABASES = {
-    'default': config('DATABASE_URL', default=default_url, cast=dburl)
+    'default': config('DATABASE_URL', default=default_dburl, cast=dburl)
 }
 
 
